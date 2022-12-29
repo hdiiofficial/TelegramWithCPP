14b85c2cde2ad6350dc3206f09c3dafbabb01128
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-29 12:24:52 +0700

    Update smkbos.cpp

--- smkbos.cpp
+++ smkbos.cpp
@@ -1,1 +1,17 @@
 #include "CTBot.h"
+CTBot HDev;
+
+void setup() {
+    Serial.Begin(9000);
+    HDev.wifiConnect("Your WiFi SSID", "Your Password Wifi");
+    HDev.setTelegramToken("Your Bot Token");
+}
+
+void loop() {
+    TBMessage msg;
+    if (HDev.getNewMessage(msg))
+        Serial.println("Cuit Cuit Sesemes dari: ");
+        Serial.Println(msg.sender.id);
+        HDev.sendMessage(msg.sender.id, "Hello");
+    delay(50);
+}
